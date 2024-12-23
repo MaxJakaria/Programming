63539c6080186106791530ec3c928b7b2c10668c
Md. Jakaria Hosen <142467940+MaxJakaria@users.noreply.github.com>
2024-12-23 17:32:03 +0600

    23/12/2024

--- Graph Theory/05. 295B - Greg and Graph.cpp
+++ Graph Theory/05. 295B - Greg and Graph.cpp
@@ -1,32 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
-
-const int N = 501;
-
 vector<int> del_order;
 vector<vector<ll>> dist;
 
 void solve(int &n){
     reverse(del_order.begin(), del_order.end());
-    
+
     vector<ll> ans;
     for(int k=0; k<n; k++){
         ll k_v = del_order[k];
 
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++)
             for(int j=1; j<=n; j++){
                 ll new_dist = dist[i][k_v] + dist[k_v][j];
                 dist[i][j] = min(dist[i][j], new_dist);
             }
-        }
 
         ll sum = 0;
-        for(int i=0; i<=k; i++){
-            for(int j=0; j<=k; j++){
+        for(int i=0; i<=k; i++)
+            for(int j=0; j<=k; j++)
                 sum += dist[del_order[i]][del_order[j]];
-            }
-        }
+
         ans.push_back(sum);
     }
 
